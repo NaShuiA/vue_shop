5b1f27fccda984d5e510c23d4228618afa131512
NaShuiA <543199311@qq.com>
2020-03-11 20:00:42 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import router from './router.js'
 import './plugins/element.js'
-
+// 导入全局样式表
+import './assets/css/global.css'
+// axios
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
 new Vue({
-  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+Vue.use(Router)
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to将要访问的路径
+  // from 代表哪个路径跳转而来
+  // next是一个函数 放行
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 从sessionStorage中获取到保存的token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果么有token,强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next()
+})
+export default router
